10506db6eba5b5c05bb7da762e6f9e626778d50e
hw_houwei <hwacdx@163.com>
2021-02-27 15:37:37 +0800

    监听生命周期事件

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver{
   int _counter = 0;
 
   void _incrementCounter() {
@@ -61,6 +61,28 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+  @override
+  @mustCallSuper
+  void initState(){
+    super.initState();
+    WidgetsBinding.instance.addObserver(this); //注册监听器
+  }
+
+  @override
+  @mustCallSuper
+  void dispose(){
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this); //移出监听器
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state){
+    print("$state");//打印生命周期事件
+    if(state == AppLifecycleState.resumed){
+      // do something
+    }
   }
 
   @override
@@ -84,17 +106,6 @@ class _MyHomePageState extends State<MyHomePage> {
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
